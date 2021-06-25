a680e348a8235d6bf3b5d71f7be3cf7541f0f266
Grzegorz Dulewicz <extern.grzegorz.dulewicz@elektrobit.com>
2021-06-25 14:00:31 +0200

    client_hello_impl_12 introduced

--- src/lib/tls/tls12/msg_client_hello_impl_12.cpp
+++ src/lib/tls/tls12/msg_client_hello_impl_12.cpp
@@ -51,34 +51,9 @@ std::vector<uint8_t> make_hello_random(RandomNumberGenerator& rng,
    }
 
 /*
-* Create a new Hello Request message
-*/
-Hello_Request::Hello_Request(Handshake_IO& io)
-   {
-   io.send(*this);
-   }
-
-/*
-* Deserialize a Hello Request message
-*/
-Hello_Request::Hello_Request(const std::vector<uint8_t>& buf)
-   {
-   if(buf.size())
-      throw Decoding_Error("Bad Hello_Request, has non-zero size");
-   }
-
-/*
-* Serialize a Hello Request message
-*/
-std::vector<uint8_t> Hello_Request::serialize() const
-   {
-   return std::vector<uint8_t>();
-   }
-
-/*
 * Create a new Client Hello message
 */
-Client_Hello::Client_Hello(Handshake_IO& io,
+Client_Hello_Impl_12::Client_Hello_Impl_12(Handshake_IO& io,
                            Handshake_Hash& hash,
                            const Policy& policy,
                            Callbacks& cb,
@@ -140,7 +115,7 @@ Client_Hello::Client_Hello(Handshake_IO& io,
 /*
 * Create a new Client Hello message (session resumption case)
 */
-Client_Hello::Client_Hello(Handshake_IO& io,
+Client_Hello_Impl_12::Client_Hello_Impl_12(Handshake_IO& io,
                            Handshake_Hash& hash,
                            const Policy& policy,
                            Callbacks& cb,
@@ -197,7 +172,7 @@ Client_Hello::Client_Hello(Handshake_IO& io,
    hash.update(io.send(*this));
    }
 
-void Client_Hello::update_hello_cookie(const Hello_Verify_Request& hello_verify)
+void Client_Hello_Impl_12::update_hello_cookie(const Hello_Verify_Request& hello_verify)
    {
    if(!m_version.is_datagram_protocol())
       throw Invalid_State("Cannot use hello cookie with stream protocol");
@@ -208,7 +183,7 @@ void Client_Hello::update_hello_cookie(const Hello_Verify_Request& hello_verify)
 /*
 * Serialize a Client Hello message
 */
-std::vector<uint8_t> Client_Hello::serialize() const
+std::vector<uint8_t> Client_Hello_Impl_12::serialize() const
    {
    std::vector<uint8_t> buf;
 
@@ -235,10 +210,10 @@ std::vector<uint8_t> Client_Hello::serialize() const
    return buf;
    }
 
-std::vector<uint8_t> Client_Hello::cookie_input_data() const
+std::vector<uint8_t> Client_Hello_Impl_12::cookie_input_data() const
    {
    if(m_cookie_input_bits.empty())
-      throw Invalid_State("Client_Hello::cookie_input_data called but was not computed");
+      throw Invalid_State("Client_Hello_Impl_12::cookie_input_data called but was not computed");
 
    return m_cookie_input_bits;
    }
@@ -246,10 +221,10 @@ std::vector<uint8_t> Client_Hello::cookie_input_data() const
 /*
 * Read a counterparty client hello
 */
-Client_Hello::Client_Hello(const std::vector<uint8_t>& buf)
+Client_Hello_Impl_12::Client_Hello_Impl_12(const std::vector<uint8_t>& buf)
    {
    if(buf.size() < 41)
-      throw Decoding_Error("Client_Hello: Packet corrupted");
+      throw Decoding_Error("Client_Hello_Impl_12: Packet corrupted");
 
    TLS_Data_Reader reader("ClientHello", buf);
 
@@ -297,7 +272,7 @@ Client_Hello::Client_Hello(const std::vector<uint8_t>& buf)
 /*
 * Check if we offered this ciphersuite
 */
-bool Client_Hello::offered_suite(uint16_t ciphersuite) const
+bool Client_Hello_Impl_12::offered_suite(uint16_t ciphersuite) const
    {
    for(size_t i = 0; i != m_suites.size(); ++i)
       if(m_suites[i] == ciphersuite)
@@ -305,7 +280,7 @@ bool Client_Hello::offered_suite(uint16_t ciphersuite) const
    return false;
    }
 
-std::vector<Signature_Scheme> Client_Hello::signature_schemes() const
+std::vector<Signature_Scheme> Client_Hello_Impl_12::signature_schemes() const
    {
    std::vector<Signature_Scheme> schemes;
 
@@ -317,21 +292,21 @@ std::vector<Signature_Scheme> Client_Hello::signature_schemes() const
    return schemes;
    }
 
-std::vector<Group_Params> Client_Hello::supported_ecc_curves() const
+std::vector<Group_Params> Client_Hello_Impl_12::supported_ecc_curves() const
    {
    if(Supported_Groups* groups = m_extensions.get<Supported_Groups>())
       return groups->ec_groups();
    return std::vector<Group_Params>();
    }
 
-std::vector<Group_Params> Client_Hello::supported_dh_groups() const
+std::vector<Group_Params> Client_Hello_Impl_12::supported_dh_groups() const
    {
    if(Supported_Groups* groups = m_extensions.get<Supported_Groups>())
       return groups->dh_groups();
    return std::vector<Group_Params>();
    }
 
-bool Client_Hello::prefers_compressed_ec_points() const
+bool Client_Hello_Impl_12::prefers_compressed_ec_points() const
    {
    if(Supported_Point_Formats* ecc_formats = m_extensions.get<Supported_Point_Formats>())
       {
@@ -340,77 +315,77 @@ bool Client_Hello::prefers_compressed_ec_points() const
    return false;
    }
 
-std::string Client_Hello::sni_hostname() const
+std::string Client_Hello_Impl_12::sni_hostname() const
    {
    if(Server_Name_Indicator* sni = m_extensions.get<Server_Name_Indicator>())
       return sni->host_name();
    return "";
    }
 
-bool Client_Hello::secure_renegotiation() const
+bool Client_Hello_Impl_12::secure_renegotiation() const
    {
    return m_extensions.has<Renegotiation_Extension>();
    }
 
-std::vector<uint8_t> Client_Hello::renegotiation_info() const
+std::vector<uint8_t> Client_Hello_Impl_12::renegotiation_info() const
    {
    if(Renegotiation_Extension* reneg = m_extensions.get<Renegotiation_Extension>())
       return reneg->renegotiation_info();
    return std::vector<uint8_t>();
    }
 
-std::vector<Protocol_Version> Client_Hello::supported_versions() const
+std::vector<Protocol_Version> Client_Hello_Impl_12::supported_versions() const
    {
    if(Supported_Versions* versions = m_extensions.get<Supported_Versions>())
       return versions->versions();
    return {};
    }
 
-bool Client_Hello::supports_session_ticket() const
+bool Client_Hello_Impl_12::supports_session_ticket() const
    {
    return m_extensions.has<Session_Ticket>();
    }
 
-std::vector<uint8_t> Client_Hello::session_ticket() const
+std::vector<uint8_t> Client_Hello_Impl_12::session_ticket() const
    {
    if(Session_Ticket* ticket = m_extensions.get<Session_Ticket>())
       return ticket->contents();
    return std::vector<uint8_t>();
    }
 
-bool Client_Hello::supports_alpn() const
+bool Client_Hello_Impl_12::supports_alpn() const
    {
    return m_extensions.has<Application_Layer_Protocol_Notification>();
    }
 
-bool Client_Hello::supports_extended_master_secret() const
+bool Client_Hello_Impl_12::supports_extended_master_secret() const
    {
    return m_extensions.has<Extended_Master_Secret>();
    }
 
-bool Client_Hello::supports_cert_status_message() const
+bool Client_Hello_Impl_12::supports_cert_status_message() const
    {
    return m_extensions.has<Certificate_Status_Request>();
    }
 
-bool Client_Hello::supports_encrypt_then_mac() const
+bool Client_Hello_Impl_12::supports_encrypt_then_mac() const
    {
    return m_extensions.has<Encrypt_then_MAC>();
    }
 
-bool Client_Hello::sent_signature_algorithms() const
+bool Client_Hello_Impl_12::sent_signature_algorithms() const
    {
    return m_extensions.has<Signature_Algorithms>();
    }
 
-std::vector<std::string> Client_Hello::next_protocols() const
+std::vector<std::string> Client_Hello_Impl_12::next_protocols() const
    {
    if(auto alpn = m_extensions.get<Application_Layer_Protocol_Notification>())
       return alpn->protocols();
    return std::vector<std::string>();
    }
 
-std::vector<uint16_t> Client_Hello::srtp_profiles() const
+std::vector<uint16_t> Client_Hello_Impl_12::srtp_profiles() const
    {
    if(SRTP_Protection_Profiles* srtp = m_extensions.get<SRTP_Protection_Profiles>())
       return srtp->profiles();
